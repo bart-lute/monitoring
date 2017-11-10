9e985f5501874e0f7ad4c9e04a098ba021ea32dd
Bart Lute <b.lute@youwe.nl>
2017-11-10 16:08:42 +0100

    working demo

--- app/src/main/java/nl/youwe/monitoring/MainActivity.java
+++ app/src/main/java/nl/youwe/monitoring/MainActivity.java
@@ -1,14 +1,27 @@
 package nl.youwe.monitoring;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
 
+    private static final String LOADING_PHRASE_CONFIG_KEY = "loading_phrase";
+    private static final String WELCOME_MESSAGE_KEY = "welcome_message";
+    private static final String WELCOME_MESSAGE_CAPS_KEY = "welcome_message_caps";
+
+    private FirebaseRemoteConfig mFirebaseRemoteConfig;
     private TextView mWelcomeTextView;
 
     @Override
@@ -21,11 +34,58 @@ public class MainActivity extends AppCompatActivity {
         Button fetchButton = findViewById(R.id.fetchButton);
         fetchButton.setOnClickListener((v) -> { fetchWelcome(); });
 
+        mFirebaseRemoteConfig = FirebaseRemoteConfig.getInstance();
+
+        FirebaseRemoteConfigSettings configSettings = new FirebaseRemoteConfigSettings.Builder()
+                .setDeveloperModeEnabled(BuildConfig.DEBUG)
+                .build();
+
+        mFirebaseRemoteConfig.setConfigSettings(configSettings);
+        mFirebaseRemoteConfig.setDefaults(R.xml.remote_config_defaults);
+
     }
 
     private void fetchWelcome() {
 
-        mWelcomeTextView.setText(mFirebaseRemoteConfig);
+        mWelcomeTextView.setText(mFirebaseRemoteConfig.getString(LOADING_PHRASE_CONFIG_KEY));
 
+        long cacheExpiration = 3600; // 1 hour
+
+        if (mFirebaseRemoteConfig.getInfo().getConfigSettings().isDeveloperModeEnabled()) {
+            Log.d(TAG, "Dev mode!");
+            cacheExpiration = 0;    // no cache in dev mode
+        }
+
+        mFirebaseRemoteConfig.fetch(cacheExpiration)
+                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(MainActivity.this, "Fetch Succeeded",
+                                    Toast.LENGTH_SHORT).show();
+
+                            mFirebaseRemoteConfig.activateFetched();
+                        } else {
+                            Toast.makeText(MainActivity.this, "Fetch Failed",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                        displayWelcomeMessage();
+                    }
+                });
+
+    }
+
+    private void displayWelcomeMessage() {
+        String welcomeMessage = mFirebaseRemoteConfig.getString(WELCOME_MESSAGE_KEY);
+
+        Log.d(TAG, "Caps: " + mFirebaseRemoteConfig.getBoolean(WELCOME_MESSAGE_CAPS_KEY));
+
+        if (mFirebaseRemoteConfig.getBoolean(WELCOME_MESSAGE_CAPS_KEY)) {
+            mWelcomeTextView.setAllCaps(true);
+        } else {
+            mWelcomeTextView.setAllCaps(false);
+        }
+
+        mWelcomeTextView.setText(welcomeMessage);
     }
 }
